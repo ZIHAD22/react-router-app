5f9f51c04bfbb832aba2dea4a05c9d657802b612
ZIHAD22 <mdzihad2244@gmail.com>
2022-03-29 02:34:30 +0600

    food show

--- src/components/FoodItems/FoodItems.jsx
+++ src/components/FoodItems/FoodItems.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Item from "../Item/Item";
 
 const FoodItems = () => {
+  const [foods, setFoods] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=Beef")
+      .then((res) => setFoods(res.data.meals));
+  }, [foods]);
   return (
-    <div>
-      <Item />
+    <div className="grid grid-cols-4 gap-4">
+      {foods.map((food) => (
+        <Item food={food} />
+      ))}
     </div>
   );
 };
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { ArrowRightIcon } from "@heroicons/react/solid";
 
-const Item = () => {
+const Item = (props) => {
+  const { strMeal, strCategory, strMealThumb } = props.food;
   return (
     <div className="w-[350px] h-[450px] m-5 shadow-2xl">
       <div className="m-2 bg-gray-300">
-        <img
-          src="https://img.freepik.com/free-photo/delicious-fried-chicken-plate_144627-27383.jpg?t=st=1648496810~exp=1648497410~hmac=5b9c7108714fa51319a636c70768bfb59234bef1299b241cdeb80ba604b56187&w=900"
-          alt=""
-        />
+        <img className="h-[200px] w-full" src={strMealThumb} alt="" />
       </div>
       <div className="m-2">
-        <h1 className="text-2xl mt-5">Teriyaki Chicken Casserole</h1>
-        <h5 className="text-[18px] mt-5">Category: Chicken</h5>
+        <h1 className="text-2xl mt-5">
+          {strMeal.length > 30 ? strMeal.slice(0, 20) + "..." : strMeal}
+        </h1>
+        <h5 className="text-[18px] mt-5">Category: {strCategory}</h5>
         <button className="flex px-4 mt-16 py-2 text-white border-2 font-bold border-[#243b48] rounded-lg mx-auto bg-[#243b48] hover:bg-white hover:text-black duration-500 hover:border-2 hover:font-bold">
           Order Now <ArrowRightIcon className="w-5 h-6 ml-3" />
         </button>
